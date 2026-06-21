f44cb97ad9e1309e9427f920005062580004ce69
Am01bathawab1 <temeke4695@gmail.com>
2026-06-21 11:51:45 +0100

    Update HeifDecoderWrapper.kt

--- app/src/main/kotlin/org/fossify/gallery/helpers/HeifDecoderWrapper.kt
+++ app/src/main/kotlin/org/fossify/gallery/helpers/HeifDecoderWrapper.kt
@@ -1,27 +1,22 @@
 package org.fossify.gallery.helpers
 
 import android.graphics.Bitmap
-import com.bumptech.glide.integration.heifdecoder.HeifDecoder
-import java.io.InputStream
+import android.graphics.ImageDecoder
+import java.io.File
 
 object HeifDecoderWrapper {
 
-    private val decoder = HeifDecoder(null) // null context is fine
-
     /**
-     * Decodes HEIC/HEIF from the stream.
+     * Decodes HEIC/HEIF from a file using Android's ImageDecoder (API 28+).
      * Returns null on failure.
      */
-    fun decode(stream: InputStream): Bitmap? {
+    fun decode(file: File): Bitmap? {
         return try {
-            decoder.decode(stream)
+            val source = ImageDecoder.createSource(file)
+            ImageDecoder.decodeBitmap(source)
         } catch (e: Exception) {
             e.printStackTrace()
             null
-        } finally {
-            try {
-                stream.close()
-            } catch (_: Exception) {}
         }
     }
 }
